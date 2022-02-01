0fbb33449fb1c001b86cddccad24d3933d622aa3
Шапил Капланов <kaplanov_shapil@outlook.com>
2022-02-01 10:43:39 +0300

    Atack

---
+++ RealExamWork/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RealExamWork.MvcApplication" Language="C#" %>
---
+++ RealExamWork/Models/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RealExamWork.Models
+{
+    public class Tovar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class MyContext : DbContext
+    {
+        public DbSet<Tovar> Tovars { get; set; }
+    }
+}
